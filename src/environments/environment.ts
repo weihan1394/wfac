16dc9660b5fb5041fdd6da18115cf6cad94f20ff
WeiHan <weihan1394@gmail.com>
2020-04-14 14:38:01 +0800

    - wip 3

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,22 +1,9 @@
 export const environment = {
   baseUrl: 'http://localhost:8080',
   production: false,
-  apiUrl: 'http://localhost:9000/keycloak-auth-api/rest',
+  // apiUrl: 'http://localhost:9000/keycloak-auth-api/rest',
 
   keycloakRealm: 'pso-seak',
   keycloakClient: 'pso-seak-app',
   keycloakBaseUrl: 'http://localhost:9000/'
 };
-
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
