e35addc530069fb7e4c53c395534a6f4d447512a
WeiHan <weihan1394@gmail.com>
2020-05-10 20:37:12 +0800

    - created post server (testing) - change proxy to multiple entries - test post data to server

--- src/app/components/dialog/dialog.component.ts
+++ src/app/components/dialog/dialog.component.ts
@@ -3,6 +3,8 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { ProvisioningItem } from '../../models/provisioning-item';
 import { ToastrService } from 'ngx-toastr';
 
+import { ProvisioningService } from '../../service/provisioning.service';
+
 @Component({
   selector: 'app-dialog',
   templateUrl: './dialog.component.html',
@@ -11,13 +13,22 @@ import { ToastrService } from 'ngx-toastr';
 export class DialogComponent {
   provisioningItem: ProvisioningItem;
 
-  constructor(public dialogRef: MatDialogRef<DialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private toastr: ToastrService) {
+  constructor(public dialogRef: MatDialogRef<DialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any, private toastr: ToastrService, private provisioningService: ProvisioningService) {
     this.provisioningItem = data.provisioning;
   }
 
   onSubmit(submission) {
     console.log(submission.data);
     console.log(submission);
+
+    this.provisioningService.sendProvisioning(submission.data).subscribe(
+      result => {
+        console.log(result);
+      },
+      err => {
+        console.log(err)
+      }
+    );
     this.dialogRef.close();
 
     this.toastr.success('', 'Provisioned created!', {
