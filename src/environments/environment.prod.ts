df51f0e8db1b68bdcd4e5a639a63b22f225714d9
WeiHan <weihan1394@gmail.com>
2020-06-04 02:00:10 +0800

    - update env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,9 @@
 export const environment = {
   production: true,
+  baseUrl: 'https://portal-wfacp.psoseak.org', 
+  keycloakRealm: 'WFAC',
+  keycloakClient: 'wfac-fe',
+  keycloakBaseUrl: 'https://idm-wfacp.psoseak.org/auth',
   backend: {
     baseURL:"https://api-wfacp.psoseak.org/api"
   }
